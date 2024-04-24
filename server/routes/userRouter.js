a2626ff359ee625bddd4c5bd6388ae5efa7c82d3
Maryem-Harmessi <hermassimyriam@gmail.com>
2024-04-24 20:02:27 +0200

    create patient model

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,7 +1,7 @@
-const express = require('express')
-const { createUser } = require('../controllers/userController')
-const router = express.Router()
+const express = require("express");
+const { createUser } = require("../controllers/userController");
+const router = express.Router();
 
-router.post('/create',createUser)
+router.post("/create", createUser);
 
-module.exports=router
+module.exports = router;
